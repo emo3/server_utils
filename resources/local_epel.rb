79b3d03b52d54fe688a740e7ae02e4a09dbc372a
Ed Overton <e.overton@gmail.com>
2019-07-21 19:15:31 -0400

    add epel repo code

---
+++ resources/local_epel.rb
@@ -0,0 +1,25 @@
+resource_name :epel_local_repo
+
+property :epel_ver, String, default: '7'
+property :web_srv,  String, default: 'http://websr'
+
+action :install do
+  # Delete any existing epel* files
+  Dir["/etc/you.repos.d/epel*"].each do |path|
+    file ::File.expand_path(path) do
+      action :delete
+    end
+  end
+
+  # create local epel repo
+  template "/etc/yum.repos.d/epel#{new_resource.epel_ver}" do
+    source 'epel.erb'
+    cookbook 'nc_tools'
+    variables(
+      epel_ver: new_resource.epel_ver,
+      web_srv:  new_resource.web_srv
+    )
+    mode 0755
+    action :create
+  end
+end
