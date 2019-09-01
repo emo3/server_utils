e353d7188cdf64be3f3213e3ac582c6b73e3576a
Ed Overton <e.overton@gmail.com>
2019-08-31 21:54:11 -0400

    fix only passing IP for web server skip deleting epel7.repo file

--- resources/local_epel.rb
+++ resources/local_epel.rb
@@ -1,12 +1,13 @@
 resource_name :epel_local_repo
 
 property :epel_ver, String, default: '7'
-property :web_srv,  String, default: 'http://10.1.1.30'
+property :web_srv,  String, default: '10.1.1.30'
 
 action :install do
   # Delete any existing epel* files
   Dir["/etc/yum.repos.d/epel*"].each do |path|
     file ::File.expand_path(path) do
+      next if file == "epel#{new_resource.epel_ver}.repo"
       action :delete
     end
   end
