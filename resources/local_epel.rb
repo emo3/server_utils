55064b8e5d2304c69315300f4576738e791b8945
Ed Overton <e.overton@gmail.com>
2019-09-01 23:00:28 -0400

    change to allow for either http or https for media

--- resources/local_epel.rb
+++ resources/local_epel.rb
@@ -1,7 +1,7 @@
 resource_name :epel_local_repo
 
 property :epel_ver, String, default: '7'
-property :web_srv,  String, default: '10.1.1.30'
+property :web_srv,  String, default: 'http://10.1.1.30'
 
 action :install do
   # create local epel repo
