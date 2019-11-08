7646f46dad2d012d979fa6a8bcd94699c60db5ca
Ed Overton <e.overton@gmail.com>
2019-11-08 18:29:52 -0500

    make some changes based on more understanding

--- metadata.rb
+++ metadata.rb
@@ -1,23 +1,16 @@
 name 'server_utils'
 maintainer 'Ed Overton'
-maintainer_email 'infuse.1301@gmail.com'
+maintainer_email 'bogus@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures server_utils'
-long_description 'Installs/Configures server_utils'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 13.0'
+
 supports 'redhat'
+supports 'centos'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/server_utils/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/emo3/server_utils/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/emo3/server_utils'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/server_utils'
 depends 'lvm'
 depends 'hostsfile'
