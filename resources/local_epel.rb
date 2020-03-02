134b757a5644b0c35da37e92329ea073b3b9ff20
Ed Overton <e.overton@gmail.com>
2020-03-01 20:43:53 -0500

    add code to allow for epel 8

--- resources/local_epel.rb
+++ resources/local_epel.rb
@@ -1,9 +1,14 @@
 resource_name :epel_local_repo
 
-property :epel_ver, String, default: '7'
 property :web_srv,  String, default: 'http://10.1.1.30'
 
 action :install do
+  epel_ver = case
+             when node['platform_version'] < '8'
+               '7'
+             else
+               '8'
+             end
   # create local epel repo
   template "/etc/yum.repos.d/epel#{new_resource.epel_ver}.repo" do
     source 'epel.erb'
