72f51c8d64a2ff094a355f308abb69fa20b9f950
Ed Overton <e.overton@gmail.com>
2020-03-01 21:33:05 -0500

    Bump version change epel_ver to local variable

--- resources/local_epel.rb
+++ resources/local_epel.rb
@@ -10,11 +10,11 @@ action :install do
                '8'
              end
   # create local epel repo
-  template "/etc/yum.repos.d/epel#{new_resource.epel_ver}.repo" do
+  template "/etc/yum.repos.d/epel#{epel_ver}.repo" do
     source 'epel.erb'
     cookbook 'server_utils'
     variables(
-      epel_ver: new_resource.epel_ver,
+      epel_ver: epel_ver,
       web_srv:  new_resource.web_srv
     )
     mode 0644
@@ -22,8 +22,8 @@ action :install do
   end
 
   # create local epel GPG file
-  template "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-#{new_resource.epel_ver}" do
-    source "RPM-GPG-KEY-EPEL-#{new_resource.epel_ver}.erb"
+  template "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-#{epel_ver}" do
+    source "RPM-GPG-KEY-EPEL-#{epel_ver}.erb"
     cookbook 'server_utils'
     mode 0644
     action :create
